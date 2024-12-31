22572ba4238acb19b046d8efc71b6d08a7479820
Kgodisho Mononyane <kgodisho.mononyane@gmail.com>
2024-12-31 12:53:31 +0200

    clean out code

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+//buttons
 const colorPicker = document.querySelector("#colorPicker");
 const colorBtn = document.querySelector("#colorBtn");
 const rainbowBtn = document.querySelector("#rainbowBtn");
 const eraserBtn = document.querySelector("#eraserBtn");
 const clearBtn = document.querySelector("#clearBtn");
+
+//grid
+const grid = document.querySelector("#grid");
