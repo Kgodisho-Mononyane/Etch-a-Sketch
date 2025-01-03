8239f7594d43d0ac6a9856a7f21be37ef7b1b88e
Kgodisho Mononyane <kgodisho.mononyane@gmail.com>
2025-01-03 08:42:47 +0200

    add button event listeners templates

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
-//slider
+//SLIDER
 const sliderValue = document.querySelector("#slider-value");
 const slider = document.querySelector("#myRange");
 
-//buttons
+//BUTTONS
 const colorPicker = document.querySelector("#colorPicker");
 const colorBtn = document.querySelector("#colorBtn");
 const rainbowBtn = document.querySelector("#rainbowBtn");
 const eraserBtn = document.querySelector("#eraserBtn");
 const clearBtn = document.querySelector("#clearBtn");
 
-//grid
+//GRID
 const grid = document.querySelector("#grid");
+
+
+
+//BUTTON EVENT LISTENERS
+colorPicker.addEventListener("click", () => {
+    //add function
+})
+
+colorBtn.addEventListener("click", () => {
+    //add function
+})
+
+rainbowBtn.addEventListener("click", () => {
+    //add function
+})
+
+eraserBtn.addEventListener("click", () => {
+    //add function
+})
+
+clearBtn.addEventListener("click", () => {
+    //add function
+})
 
