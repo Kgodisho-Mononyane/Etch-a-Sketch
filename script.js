f3a7dbabd1a27f011d8c1c97862b5ce7639cecb4
Kgodisho Mononyane <kgodisho.mononyane@gmail.com>
2025-01-05 08:39:33 +0200

    change grid id name

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const eraserBtn = document.querySelector("#eraserBtn");
 const clearBtn = document.querySelector("#clearBtn");
 
 //GRID
-const grid = document.querySelector("#grid-container");
+const grid = document.querySelector("#grid");
 
 let rows = 16;
 let cols = 16;
@@ -21,32 +21,58 @@ for (let i = 0; i < rows * cols; i++) {
     grid.appendChild(cell)
 }
 
-console.log(slider.value)
-
 //BUTTON EVENT LISTENERS
+
+colorPicker.addEventListener("click", () => {
+    pickColor();
+})
+
+colorBtn.addEventListener("click", () => {
+    colorFunction();
+})
+
+rainbowBtn.addEventListener("click", () => {
+    rainbowFunction();
+})
+
+eraserBtn.addEventListener("click", () => {
+    eraser();
+})
+
+clearBtn.addEventListener("click", () => {
+    clear();
+})
+
 // cell.addEventListener("click", () => {
     
 // })
 
-colorPicker.addEventListener("click", () => {
-    //add function
-})
-
-colorBtn.addEventListener("click", () => {
-    //add function
-})
-
-rainbowBtn.addEventListener("click", () => {
-    //add function
-})
-
-eraserBtn.addEventListener("click", () => {
-    //add function
-})
-
-clearBtn.addEventListener("click", () => {
-    //add function
-})
-
+//SLIDER
+sliderValue.textContent = `${slider.value} x ${slider.value}`;
 
 //FUNCTIONS
+
+function pickColor() {
+    //function that changes the colour that is picked
+    console.log("You clicked the color changer")
+}
+
+function colorFunction() {
+    //function that changes the setting to the colour setting
+    console.log("You clicked the color button")
+}
+
+function rainbowFunction() {
+    // function that changes the setting to the rainbow setting
+    console.log("You clicked the rainbow button")
+}
+
+function eraser() {
+    // function that changes the setting to the eraser setting
+    console.log("You clicked the eraser setting")
+}
+
+function clear() {
+    //function that clears all changes back to the default
+    console.log("You clicked the clear button")
+}
