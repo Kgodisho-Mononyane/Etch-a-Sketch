885a6bf8bbf82c7c07809b545ee0079d2d281015
Kgodisho Mononyane <kgodisho.mononyane@gmail.com>
2025-01-06 08:35:20 +0200

    add comments to the functions

--- script.js
+++ script.js
@@ -24,7 +24,6 @@ const grid = document.querySelector("#grid");
 
 
 //CREATE THE GRID
-function createGrid() {
     let rows = 16;
     let cols = 16;
     
@@ -38,9 +37,7 @@ function createGrid() {
         //add event listener that adds colour based on a function that chooses the colour
         grid.appendChild(cell)
 }
-}
 
-createGrid();
 
 
 //BUTTON EVENT LISTENERS
@@ -89,27 +86,34 @@ function changeSliderValue() {
 
 function pickColor() {
     //function that changes the colour that is picked
+    //get a colour from the color picker
     console.log("You clicked the color changer")
 }
 
 function colorFunction() {
     //function that changes the setting to the colour setting
+    //change the setting to the color setting
+    //get the color chosen with the colour picker
+    // change the div background color to the chosen colour
     console.log("You clicked the color button")
 }
 
 function rainbowFunction() {
     // function that changes the setting to the rainbow setting
+    //change the setting to a rainbow setting
+    //change the div colours in a random rainbow of colours
     console.log("You clicked the rainbow button")
 }
 
 function eraser() {
     // function that changes the setting to the eraser setting
+    //remove the color added to a div
     cell.style.backgroundColor = ''
     console.log("You clicked the eraser setting")
 }
 
 function clearGrid() {
-    //function that clears all changes back to the default
-    grid.innerHTML = ''
+    //clear out the entire grid back to the default
+    //grid.innerHTML = ''
     console.log("You clicked the clear button")
 }
