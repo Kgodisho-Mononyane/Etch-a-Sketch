618d015b468db4fc28b9c5e0e94c0f83160917df
Kgodisho Mononyane <kgodisho.mononyane@gmail.com>
2025-01-22 10:20:22 +0200

    remove old functions

--- script.js
+++ script.js
@@ -40,13 +40,13 @@ clearBtn.addEventListener("click", () => {
 
 //GRID
 let rows = 16;
-    let cols = 16;
+let cols = 16;
 
-    for (let i = 0; i < rows * cols; i++) {
-        const cell = document.createElement("div");
-        cell.classList.add("grid-cell");
-        cell.addEventListener("mousedown", () => {
-            cell.style.backgroundColor = "red";
+for (let i = 0; i < rows * cols; i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("grid-cell");
+    cell.addEventListener("mousedown", () => {
+    cell.style.backgroundColor = "red";
         });
     }
     grid.appendChild(cell);
@@ -54,6 +54,15 @@ let rows = 16;
 
 
 //FUNCTIONS
+
+function currentColor() {
+}
+
+function currentMode() {
+}
+
+function currentSize() {
+}
 
 function pickColor() {
 }
@@ -70,57 +79,6 @@ function eraserFunction() {
 function clearFunction() {
 }
 
-
-
-// //FUNCTIONS
-// function currentColor() {
-//     //function that establishes the current colour
-//     /* */
-// }
-
-// function currentMode() {
-//     //function that sets the current mode
-// }
-
-// function currentSize() {
-//     //function that sets the current size
-// }
-
-// function changeSliderValue() {
-//     // function that changes the value above the slider
-//     sliderValue.textContent = `${slider.value} x ${slider.value}`;
-// }
-
-// function pickColor() {
-//     //function that changes the colour that is picked
-//     //get a colour from the color picker
-//     console.log("You clicked the color changer")
-// }
-
-// function colorFunction() {
-//     //function that changes the setting to the colour setting
-//     //change the setting to the color setting
-//     //get the color chosen with the colour picker
-//     // change the div background color to the chosen colour
-//     console.log("You clicked the color button")
-// }
-
-// function rainbowFunction() {
-//     // function that changes the setting to the rainbow setting
-//     //change the setting to a rainbow setting
-//     //change the div colours in a random rainbow of colours
-//     console.log("You clicked the rainbow button")
-// }
-
-// function eraser() {
-//     // function that changes the setting to the eraser setting
-//     //remove the color added to a div
-//     cell.style.backgroundColor = ''
-//     console.log("You clicked the eraser setting")
-// }
-
-// function clearGrid() {
-//     //clear out the entire grid back to the default
-//     //grid.innerHTML = ''
-//     console.log("You clicked the clear button")
-// }
+function sliderValue() {
+    sliderValue.textContent = `${slider.value} x ${slider.value}`;
+}
